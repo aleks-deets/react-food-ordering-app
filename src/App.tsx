7e38537ebf5ea05fcfcd5bb674436a6b8d3cd083
metaphysical_light <metacat.me@gmail.com>
2024-03-29 01:01:09 +0300

    Mugrated navigation from jsx-elements to createBrowserRouter()

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { MouseEvent, useState } from "react";
-import { Routes, Route } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Menu } from "./pages/Menu/Menu";
 import { Cart } from "./pages/Cart/Cart";
 import { Error } from "./pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -25,11 +40,7 @@ function App() {
         <a href="/">Меню</a>
         <a href="/cart">Корзина</a>
       </div>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
