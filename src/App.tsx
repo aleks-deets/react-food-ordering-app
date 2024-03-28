3adb387c1da82eb1c4a8c3c46de58fb9b51ddd21
metaphysical_light <metacat.me@gmail.com>
2024-03-28 16:35:36 +0300

    Added navigation. Installed react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -17,6 +21,15 @@ function App() {
         Button
       </Button>
       <Input placeholder="Email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
