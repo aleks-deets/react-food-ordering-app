57e7c471799f53ecd7b6d03e5fe97f1581e42617
metaphysical_light <metacat.me@gmail.com>
2024-03-29 01:39:42 +0300

    Created Outlet from react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
 import { MouseEvent, useState } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Menu } from "./pages/Menu/Menu";
-import { Cart } from "./pages/Cart/Cart";
-import { Error } from "./pages/Error/Error";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-]);
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -36,11 +17,6 @@ function App() {
         Button
       </Button>
       <Input placeholder="Email" />
-      <div>
-        <a href="/">Меню</a>
-        <a href="/cart">Корзина</a>
-      </div>
-      <RouterProvider router={router} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import App from "./App.tsx";
 import "./index.css";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
+import { Layout } from "./layout/Layout/Layout.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
