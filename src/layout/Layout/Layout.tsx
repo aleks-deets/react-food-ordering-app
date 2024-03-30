86c6cbf26f0bff346a631a1cbe2c00c7de177a13
metaphysical_light <metacat.me@gmail.com>
2024-03-30 16:28:43 +0300

    Replaced hook useLocation with NavLink.

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,16 +1,9 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Button from "../../components/Button/Button";
-import { useEffect } from "react";
 import cn from "classnames";
 
 export function Layout() {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
-
   return (
     <div className={styles["layout"]}>
       <div className={styles["sidebar"]}>
@@ -25,19 +18,28 @@ export function Layout() {
           <div className={styles["email"]}>email@gmail.com</div>
         </div>
         <div className={styles["menu"]}>
-          <Link
+          <NavLink
             to="/"
-            className={cn(styles["link"], {
-              [styles.active]: location.pathname === "/",
-            })}
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: isActive,
+              })
+            }
           >
             <img src="/menu-icon.svg" alt="Menu icon" />
             Меню
-          </Link>
-          <Link to="/cart" className={styles["link"]}>
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: isActive,
+              })
+            }
+          >
             <img src="/cart-icon.svg" alt="Cart icon" />
             Корзина
-          </Link>
+          </NavLink>
         </div>
         <Button className={styles["exit"]}>
           <img src="/exit-icon.svg" alt="Exit icon" />
