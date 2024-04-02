9436f5182fd8eced502e2afeb385374a77210f71
metaphysical_light <metacat.me@gmail.com>
2024-04-02 14:50:34 +0300

    Made lazy loading of Menu component using react lazy and Suspense.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // import App from "./App.tsx";
 import "./index.css";
-import { Menu } from "./pages/Menu/Menu";
 import { Cart } from "./pages/Cart/Cart";
 import { Error as ErrorPage } from "./pages/Error/Error"; // Чтобы не было конфликтов
 import { Layout } from "./layout/Layout/Layout.tsx";
 import { Product } from "./pages/Product/Product.tsx";
 import axios from "axios";
 import { PREFIX } from "./helpers/API.ts";
+
+const Menu = lazy(() => import("./pages/Menu/Menu"));
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Menu />,
+        element: (
+          <Suspense fallback={<>Loading....</>}>
+            <Menu />
+          </Suspense>
+        ),
       },
       {
         path: "/cart",
