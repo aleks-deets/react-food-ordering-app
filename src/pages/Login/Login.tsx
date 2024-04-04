6d4fa2bc2ba9a739fba1091782176881d3ad107c
metaphysical_light <metacat.me@gmail.com>
2024-04-04 21:13:02 +0300

    Added logic for sending POST-request to API server for Login page. Handled case when request fails with an error.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,53 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
 import Button from "../../components/Button/Button";
 import Headling from "../../components/Headling/Headling";
 import Input from "../../components/Input/Input";
+import axios, { AxiosError } from "axios";
+import { PREFIX } from "../../helpers/API";
+
+export type LoginForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
 
 export function Login() {
-  const submit = (e: FormEvent) => {
+  const [error, setError] = useState<string | null>();
+
+  const submit = async (e: FormEvent) => {
     e.preventDefault();
-    console.log(e);
+    setError(null);
+    //console.log(e);
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    //console.log(email.value + " " + password.value);
+    await sendLogin(email.value, password.value);
+  };
+
+  const sendLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post(`${PREFIX}/auth/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        //console.log(e);
+        setError(e.response?.data.message);
+      }
+    }
   };
 
   return (
     <div className={styles["login"]}>
       <Headling>Sign in</Headling>
+      {error && <div className={styles["error"]}>{error}</div>}
       <form className={styles["form"]} onSubmit={submit}>
         <div className={styles["field"]}>
           <label htmlFor="email">Enter email</label>
@@ -31,6 +65,8 @@ export function Login() {
         <Button appearance="big">Sign in</Button>
       </form>
       <div className={styles["links"]}>
+        {/* <div>Don't have an account?</div>
+        <Link to="/auth/register">Sign up</Link> */}
         <div>New to our platform?</div>
         <Link to="/auth/register">Sign up now.</Link>
       </div>
