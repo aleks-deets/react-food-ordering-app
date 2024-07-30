a4eb8452b75eb199a0645ca0ea106f3881412fbb
metaphysical_light <metacat.me@gmail.com>
2024-07-30 13:43:04 +0300

    Created cart slice and connected it to store. Added ability to add items to product cart.

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -11,6 +11,7 @@ export function Layout() {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const profile = useSelector((s: RootState) => s.user.profile);
+  const items = useSelector((s: RootState) => s.cart.items);
 
   useEffect(() => {
     dispatch(getProfile());
@@ -61,6 +62,7 @@ export function Layout() {
             <img src="/cart-icon.svg" alt="Cart icon" />
             Корзина
           </NavLink>
+          {items.reduce((acc, item) => (acc += item.count), 0)}
         </div>
         <Button className={styles["exit"]} onClick={logout}>
           <img src="/exit-icon.svg" alt="Exit icon" />
