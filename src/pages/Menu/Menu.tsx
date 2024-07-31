6738a443d5d39fb60c7457dce1104aea88705123
Aleks Dee <metacat.me@gmail.com>
2024-07-31 17:21:04 +0300

    Added logic for searchbar to find products by their ingredients.

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
 import Headling from "../../components/Headling/Headling";
 import Search from "../../components/Search/Search";
@@ -11,11 +11,20 @@ export function Menu() {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | undefined>();
+  const [filter, setFilter] = useState<string>();
 
-  const getMenu = async () => {
+  useEffect(() => {
+    getMenu(filter);
+  }, [filter]);
+
+  const getMenu = async (name?: string) => {
     try {
       setIsLoading(true);
-      const { data } = await axios.get<IProduct[]>(`${PREFIX}/products`);
+      const { data } = await axios.get<IProduct[]>(`${PREFIX}/products`, {
+        params: {
+          name,
+        },
+      });
       setProducts(data);
       setIsLoading(false);
     } catch (e) {
@@ -28,19 +37,25 @@ export function Menu() {
     }
   };
 
-  useEffect(() => {
-    getMenu();
-  }, []);
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
 
   return (
     <>
       <div className={styles["head"]}>
         <Headling>Menu</Headling>
-        <Search placeholder="Введите блюдо или состав" />
+        <Search
+          placeholder="Введите блюдо или состав"
+          onChange={updateFilter}
+        />
       </div>
       <div>
         {error && <>{error}</>}
-        {!isLoading && <MenuList products={products} />}
+        {!isLoading && products.length > 0 && <MenuList products={products} />}
+        {!isLoading && products.length === 0 && (
+          <>Не найдено товаров по запросу</>
+        )}
         {isLoading && <>is loading...</>}
       </div>
     </>
