fad495608a5f41eb8a31bb80f78f355e63859ae1
light-text <metacat.me@gmail.com>
2024-08-06 00:24:40 +0300

    Styled display of number of items in the cart.

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -60,9 +60,11 @@ export function Layout() {
             }
           >
             <img src="/cart-icon.svg" alt="Cart icon" />
-            Корзина
+            Корзина{" "}
+            <span className={styles["cart-count"]}>
+              {items.reduce((acc, item) => (acc += item.count), 0)}
+            </span>
           </NavLink>
-          {items.reduce((acc, item) => (acc += item.count), 0)}
         </div>
         <Button className={styles["exit"]} onClick={logout}>
           <img src="/exit-icon.svg" alt="Exit icon" />
